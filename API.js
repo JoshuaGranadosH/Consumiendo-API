1b9bac86093b12113a39557cd05602d7c8b920ab
Josh Granados <josh_ramstein@hotmail.com>
2021-04-08 16:24:39 -0500

    Mod .js and .html

--- API.js
+++ API.js
@@ -1,6 +1,6 @@
 const marvel = {
   render: () => {
-    const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=XXX&hash=XXX';
+    const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=8bd4274649a997ad75871afc866acc75&hash=55c4dfe45fa2a68689f4954ad1d3685d';
     const container = document.querySelector('#marvel-row');
     let contentHTML = '';
 
